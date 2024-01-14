b8d67e7a5662a688abd62c6f20c97a2c822eeec1
Mohammednur <mamebb2023@gmail.com>
2024-01-14 18:30:40 +0300

    Filtered Logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -12,3 +12,25 @@ def filter_datum(fields: List[str], redaction: str, message: str, separator: str
     for data in fields:
         message = re.sub(fr'{data}=.+?{separator}', f'{data}={redaction}{separator}', message)  # nopep8
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields: List[str] = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ Format the log input
+        """
+        return filter_datum(
+                self.fields,
+                self.REDACTION,
+                super(RedactingFormatter, self).format(record),
+                self.SEPARATOR)
