5945f875566ee862a5c83d58e7a75028c053ba84
Mohammednur <mamebb2023@gmail.com>
2024-01-15 11:46:33 +0300

    Encrypt Password

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+""" Encrypt Password
+"""
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """ Hashes a users password
+    """
+    b_hash = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+    return b_hash
+
+
+def is_valid(hashed_password: bytes, password: int) -> bool:
+    """ Checks if hashed password and password matches
+    """
+    if bcrypt.checkpw(password.encode('utf-8'), hashed_password):
+        return True
+    return False
