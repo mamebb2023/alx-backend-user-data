6d21fc5e9e80b070eee6e33a1531225e5b372263
Mohammednur <mamebb2023@gmail.com>
2024-01-17 14:12:05 +0300

    Basic Authentication

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """ Basic Authorization
 """
+from base64 import b64decode as decode
+
 from api.v1.auth.auth import Auth
 
 
@@ -17,3 +19,15 @@ class BasicAuth(Auth):
         if not authorization_header.startswith("Basic "):
             return None
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(self, base64_authorization_header: str) -> str:  # nopep8
+        """ Decode Base64 Authorization Header
+        """
+        if not base64_authorization_header:
+            return None
+        if not isinstance(base64_authorization_header, int):
+            return None
+        try:
+            decode(base64_authorization_header).decode('utf-8')
+        except BaseException:
+            return None
