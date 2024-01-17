6bde545f2712451332484ffefb3b3f63bfb6183d
Mohammednur <mamebb2023@gmail.com>
2024-01-17 14:03:52 +0300

    Basic Authentication

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -7,3 +7,13 @@ from api.v1.auth.auth import Auth
 class BasicAuth(Auth):
     """ Basic Authorization class
     """
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:  # nopep8
+        """ Extract Base64 Authorization
+        """
+        if not authorization_header:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith("Basic "):
+            return None
+        return authorization_header[6:]
