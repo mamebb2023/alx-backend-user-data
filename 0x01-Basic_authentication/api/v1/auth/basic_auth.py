980ac177d002297ff3bb078009f67bfa26ec3cda
Mohammednur <mamebb2023@gmail.com>
2024-01-17 15:54:48 +0300

    Basic Authentication

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -70,3 +70,15 @@ class BasicAuth(Auth):
         if not user[0].is_valid_password(user_pwd):
             return None
         return user[0]
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ Current User Identification
+        """
+        auth_hdr = self.authorization_header(request)
+        extract_auth_hdr = self.extract_base64_authorization_header(auth_hdr)
+        decode_auth_hdr = self.decode_base64_authorization_header(extract_auth_hdr)
+        usr_credentials = self.extract_user_credentials(decode_auth_hdr)
+        usr_email = usr_credentials[0]
+        usr_pwd = usr_credentials[1]
+        user = self.user_object_from_credentials(usr_email, usr_pwd)
+        return user
