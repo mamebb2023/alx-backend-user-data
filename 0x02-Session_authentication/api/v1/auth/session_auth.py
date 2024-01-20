ef31ae2fba08caf38d3d51f980f0065f71bed584
Mohammednur <mamebb2023@gmail.com>
2024-01-20 18:16:00 +0300

    Session Authentication

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -39,3 +39,20 @@ class SessionAuth(Auth):
         s_id = self.session_cookie(request)
         user_id = self.user_id_for_session_id(s_id)
         return User.get(user_id)
+
+    def destroy_session(self, request=None):
+        """ Destroy session
+        """
+        if not request:
+            return False
+        
+        session_id = self.session_cookie(request)
+        if not session_id:
+            return False
+        
+        user_id = self.user_id_for_session_id(session_id)
+        if not user_id:
+            return False
+
+        self.user_id_by_session_id.pop(session_id)
+        return True
--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -10,7 +10,7 @@ from models.user import User
 
 @app_views.route("/auth_session/login", methods=["POST"], strict_slashes=False)
 def usr_login():
-    """ User Session Login
+    """ User Login
     """
     email = request.form.get('email')
     pwd = request.form.get('password')
@@ -33,3 +33,17 @@ def usr_login():
     r = jsonify(valid_user.to_json())
     r.set_cookie(getenv("SESSION_NAME"), s_id)
     return r
+
+
+@app_views.route("/auth_session/logout", methods=["DELETE"], strict_slashes=False)
+def usr_logout():
+    """ User Logout
+    """
+    from api.v1.app import auth
+
+    delete = auth.destroy_session(request)
+    
+    if delete is False:
+        abort(404)
+
+    return jsonify({}), 200
