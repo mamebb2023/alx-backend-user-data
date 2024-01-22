82574e5d4e842b207d99729703e4e85266aeb1fa
Mohammednur <mamebb2023@gmail.com>
2024-01-22 11:02:52 +0300

    Users Database

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+""" User database (SQLAlchemy)
+"""
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base =  declarative_base()
+
+
+class User(Base):
+    """ Class for user database
+    """
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    session_id = Column(String, nullable=False)
+    reset_token = Column(String, nullable=False)
