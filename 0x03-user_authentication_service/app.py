f8538977216565cf1c206f6be104857f10e017da
Mohammednur <mamebb2023@gmail.com>
2024-01-23 10:12:18 +0300

    Flask API

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -3,14 +3,34 @@
 """
 from flask import Flask, jsonify
 
+from auth import Auth
 
 app = Flask(__name__)
+AUTH = Auth()
+
 
 @app.route("/", strict_slashes=False)
 def index():
     """ index page
     """
-    return jsonify({"message": "Bienvenue"})
+    return jsonify(message="Bienvenue")
+
+
+@app.route("/users", methods=["POST"], strict_slashes=False)
+def register_user():
+    """ Register user
+    """
+    email = request.form.get("email")
+    pwd = request.form.get("password")
+
+    if email is None or pwd is None:
+        return
+
+    try:
+        AUTH.register_user(email, pwd)
+        return jsonfy(email=email, message="user created")
+    except ValueError:
+        return jsonfy(message="email already registered"), 400
 
 
 if __name__ == "__main__":
