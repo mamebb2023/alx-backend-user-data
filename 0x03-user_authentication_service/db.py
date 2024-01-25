b5f9c1bcef62316ace249e0bbe3da3f64442d2fb
Mohammednur <mamebb2023@gmail.com>
2024-01-25 11:56:04 +0300

    Users Database

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -34,42 +34,12 @@ class DB:
 
     def add_user(self, email: str, hashed_password: str) -> User:
         """ User Registration
+        Args:
+            email (str): the user email
+            hashed_password (str): the user hashed password
         """
         new_user = User(email=email, hashed_password=hashed_password)
-        try:
-            self._session.add(new_user)
-            self._session.commit()
-        except Exception as e:
-            print(f"Error adding user to database: {e}")
-            self._session.rollback()
-            raise
-        return new_user
-
-    def find_user_by(self, **kwargs) -> User:
-        """ Find a user by arguments
-        """
-        for arg in kwargs.keys():
-            if not hasattr(User, arg):
-                raise InvalidRequestError
-
-        user = self._session.query(User).filter_by(**kwargs).first()
-        if user is None:
-            raise NoResultFound
-
-        return user
-
-    def update_user(self, user_id: int, **kwargs) -> None:
-        """ Update User
-        """
-        user = None
-        try:
-            user = self.find_user_by(id=user_id)
-        except NoReslutFound:
-            return
-
-        for k, v in kwargs.items():
-            if k not in user.__dict__:
-                raise ValueError
-            setattr(user, k, v)
-        self._session.add(user)
+        self._session.add(new_user)
         self._session.commit()
+
+        return new_user
