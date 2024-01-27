1b3ab00d53547063435e3b9406304e3ae9f746d5
Mohammednur <mamebb2023@gmail.com>
2024-01-27 10:31:40 +0300

    Users Database

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -46,3 +46,15 @@ class DB:
         self._session.commit()
 
         return new_user
+
+    def find_user_by(self, **kwargs):
+        """ Finds a user by some arguments
+        """
+        for arg in kwargs.keys():
+            if not hasattr(User, arg):
+                raise InvalidRequestError
+
+        user = self._session.query(User).filter_by(**kwargs).first()
+        if user is None:
+            raise NoResultFound
+        return user
